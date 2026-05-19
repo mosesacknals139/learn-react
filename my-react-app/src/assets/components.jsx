f14e0414a12ab0d471a59a6d3cd36f5de95c1d20
mosesacknals139 <mooses1397@gmail.com>
2026-05-19 16:57:27 +0530

    components.jsx

---
+++ my-react-app/src/assets/components.jsx
@@ -0,0 +1,9 @@
+function Header(props) {
+    return (
+        <div>
+            <header>hi nice to meet you</header>
+            <h2>{props.number}</h2>
+        </div>
+    );
+}
+export default Header;
