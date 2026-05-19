bb074de4dfca78159e928edaa19658453f70605e
mosesacknals139 <mooses1397@gmail.com>
2026-05-19 16:32:43 +0530

    my learn-react first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,9 @@
+import Header from './assets/Header.jsx'
+function App() {
+  return (
+    <Header />
+  );
+
+}
+
+export default App
---
+++ my-react-app/src/assets/Header.jsx
@@ -0,0 +1,10 @@
+function Card() {
+    return (
+        <header>
+            <h1 >{number}</h1>
+        </header>
+    );
+}
+
+<h1><Card number="1241241" /></h1>
+export default Header;
